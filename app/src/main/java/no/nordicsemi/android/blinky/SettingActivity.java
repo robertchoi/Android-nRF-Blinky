3f2fb402302d48d867b39c95b33fecbf3bd9a4ec
robertchoi <1234>
2021-06-14 22:31:02 +0900

    change setting page

--- app/src/main/java/no/nordicsemi/android/blinky/SettingActivity.java
+++ app/src/main/java/no/nordicsemi/android/blinky/SettingActivity.java
@@ -6,20 +6,39 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class SettingActivity extends AppCompatActivity {
 
-    public int p1,p2,p3,p4,p5;
+    public int p1,p11,p12,p2,p3,p4,p5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_setting);
+
+        LinearLayout sl0 = findViewById(R.id.sl0);
+        LinearLayout sl1 = findViewById(R.id.sl1);
+        LinearLayout sl2 = findViewById(R.id.sl2);
+
+        ViewGroup.LayoutParams params = sl0.getLayoutParams();
+
+        Button sl1Button = findViewById(R.id.sl1Button);
+        Button sl2Button = findViewById(R.id.sl2Button);
+
+        EditText editTextNumber1 = findViewById(R.id.editTextNumber1);
+        EditText editTextNumber2 = findViewById(R.id.editTextNumber2);
 
         final MaterialToolbar toolbar = findViewById(R.id.toolbarMain);
         /* toolbar.setTitle("LUHELO"); */
@@ -30,6 +49,8 @@ public class SettingActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPref.edit();
 
         p1 = sharedPref.getInt(getString(R.string.preset1), 0);
+        p11 = sharedPref.getInt("preset11", 5);
+        p12 = sharedPref.getInt("preset12", 2);
         p2 = sharedPref.getInt(getString(R.string.preset2), 0);
         p3 = sharedPref.getInt(getString(R.string.preset3), 0);
         p4 = sharedPref.getInt(getString(R.string.preset4), 0);
@@ -42,10 +63,19 @@ public class SettingActivity extends AppCompatActivity {
         CheckBox cb4 = findViewById(R.id.checkBox4);
         CheckBox cb5 = findViewById(R.id.checkBox5);
 
-        if(p1 == 1)
+        if(p1 == 1) {
             cb1.setChecked(true);
-        else
+            sl1.setVisibility(View.VISIBLE);
+            sl2.setVisibility(View.VISIBLE);
+        }
+        else {
             cb1.setChecked(false);
+            sl1.setVisibility(View.GONE);
+            sl2.setVisibility(View.GONE);
+        }
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        sl0.setLayoutParams(params);
 
         if(p2 == 1)
             cb2.setChecked(true);
@@ -64,6 +94,28 @@ public class SettingActivity extends AppCompatActivity {
         else
             cb5.setChecked(false);
 
+        editTextNumber1.setText(String.valueOf(p11));
+        editTextNumber2.setText(String.valueOf(p12));
+
+
+        sl1Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "알람 반복 간격을 설정하였습니다.", Toast.LENGTH_SHORT).show();
+
+                p11= Integer.parseInt(String.valueOf(editTextNumber1.getText()));
+                editor.putInt(getString(R.string.preset11), p11);
+                editor.commit();
+            }
+        });
+        sl2Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "알람 반복 횟수를 설정하였습니.", Toast.LENGTH_SHORT).show();
+                p12= Integer.parseInt(String.valueOf(editTextNumber2.getText()));
+                editor.putInt(getString(R.string.preset12), p12);
+                editor.commit();
+            }
+        });
+
 
         cb1.setOnClickListener(new CheckBox.OnClickListener() {
             @Override
@@ -71,9 +123,17 @@ public class SettingActivity extends AppCompatActivity {
                 // TODO : process the click event.
                 if(cb1.isChecked()){
                     p1 = 1;
+                    sl1.setVisibility(View.VISIBLE);
+                    sl2.setVisibility(View.VISIBLE);
                 } else {
                     p1 = 0;
+                    sl1.setVisibility(View.GONE);
+                    sl2.setVisibility(View.GONE);
                 }
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                sl0.setLayoutParams(params);
+
                 editor.putInt(getString(R.string.preset1), p1);
                 editor.commit();
             }
@@ -134,6 +194,9 @@ public class SettingActivity extends AppCompatActivity {
                 editor.commit();
             }
         });
+
+
+
 
     }
 }
